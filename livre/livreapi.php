65a83b12180d0f3adbf434530361d7b4a10e6179
sara3118 <72915679+sara3118@users.noreply.github.com>
2020-10-29 14:36:49 +0100

    Update livreapi.php

--- livre/livreapi.php
+++ livre/livreapi.php
@@ -1,15 +1,20 @@
 <?php
 include "../includes/database.php";
  include "../includes/functions.php";
- $titre = securisation(@$_GET['titre']);
- $sql = "SELECT * FROM livre WHERE $titre LIKE '%".$titre."%'";
+  $titre = securisation(@$_GET['titre']);
+ $sql = "SELECT * FROM livre WHERE titre LIKE '%".$titre."%'";
 
     $sth = $dbco->prepare($sql);
+    $sth->execute();
     $result = $sth->fetchAll(PDO::FETCH_ASSOC);
     $livreList=array();
     foreach ($result as $row => $livre) {
         $livreList[]=$livre;
+
+
     }
-    echo json_encode($livreList);
+
+    $data=json_encode($result);
+    print_r($data);
 
 ?>
